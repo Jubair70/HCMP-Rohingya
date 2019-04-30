991d7032b8c3b039d19ca0a1c34ce14b4cfa9174
Emtiaz Md. Sazzad Hossain <sazzad@mpower-social.com>
2019-04-30 11:46:36 +0600

    Activity progress site Improvement -- Edit : V-1

--- kobocat/onadata/apps/hcmp_report/urls.py
+++ kobocat/onadata/apps/hcmp_report/urls.py
@@ -65,6 +65,7 @@ urlpatterns = patterns('',
 
                        url(r'^activity_progress_edit/(?P<id_string>[^/]+)/(?P<instance_id>\d+)/$', views.activity_progress_edit, name='activity_progress_edit'),
 
+
                        url(r'^get_opt_activity_list/(?P<id_string>[^/]+)/(?P<donor>[^/]+)/$', views.get_opt_activity_list, name='get_opt_activity_list'),
                        url(r'^get_opt_sub_activity_list/(?P<id_string>[^/]+)/(?P<donor>[^/]+)/(?P<activity>[^/]+)/$',
                            views.get_opt_sub_activity_list, name='get_opt_sub_activity_list'),
